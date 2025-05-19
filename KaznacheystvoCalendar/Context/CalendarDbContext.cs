e6cfb15753e859ca270fff44a1fda0c5197b6647
Igor Pavlov <igor3356c@gmail.com>
2025-05-20 00:11:20 +0300

    context

---
+++ KaznacheystvoCalendar/Context/CalendarDbContext.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaznacheystvoCalendar.Models;
+
+public partial class CalendarDbContext : DbContext
+{
+    public CalendarDbContext()
+    {
+    }
+
+    public CalendarDbContext(DbContextOptions<CalendarDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<EventMember> EventMembers { get; set; }
+
+    public virtual DbSet<EventVisible> EventVisibles { get; set; }
+
+    public virtual DbSet<Location> Locations { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("C")
+            .HasPostgresEnum("statuses", new[] { "Предстоящее", "Идёт", "Завершено" });
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("comment_pkey");
+
+            entity.ToTable("comment");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateTime)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("date_time");
+            entity.Property(e => e.EventId).HasColumnName("event_id");
+            entity.Property(e => e.Text)
+                .HasMaxLength(2048)
+                .HasColumnName("text");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.EventId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("fk_event");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("comment_user_id_fkey");
+        });
+
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("unit_pkey");
+
+            entity.ToTable("department");
+
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("nextval('unit_id_seq'::regclass)")
+                .HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("event_pkey");
+
+            entity.ToTable("event");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(2055)
+                .HasColumnName("description");
+            entity.Property(e => e.EndDateTime)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("end_date_time");
+            entity.Property(e => e.LocationId).HasColumnName("location_id");
+            entity.Property(e => e.ManagerId).HasColumnName("manager_id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(150)
+                .HasColumnName("name");
+            entity.Property(e => e.StartDateTime)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("start_date_time");
+            entity.Property(e => e.Status)
+                .HasMaxLength(30)
+                .HasColumnName("status");
+
+            entity.HasOne(d => d.Location).WithMany(p => p.Events)
+                .HasForeignKey(d => d.LocationId)
+                .OnDelete(DeleteBehavior.SetNull)
+                .HasConstraintName("event_place_id_fkey");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.Events)
+                .HasForeignKey(d => d.ManagerId)
+                .HasConstraintName("event_manager_id_fkey");
+        });
+
+        modelBuilder.Entity<EventMember>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("event_members_pkey");
+
+            entity.ToTable("event_members");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.EventId).HasColumnName("event_id");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.EventMembers)
+                .HasForeignKey(d => d.EventId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("event_members_event_id_fkey");
+
+            entity.HasOne(d => d.User).WithMany(p => p.EventMembers)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("event_members_user_id_fkey");
+        });
+
+        modelBuilder.Entity<EventVisible>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("event_visible_pkey");
+
+            entity.ToTable("event_visible");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DepartmentId).HasColumnName("department_id");
+            entity.Property(e => e.EventId).HasColumnName("event_id");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.EventVisibles)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("event_visible_unit_id_fkey");
+
+            entity.HasOne(d => d.Event).WithMany(p => p.EventVisibles)
+                .HasForeignKey(d => d.EventId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("event_visible_event_id_fkey");
+        });
+
+        modelBuilder.Entity<Location>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("place_pkey");
+
+            entity.ToTable("location");
+
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("nextval('place_id_seq'::regclass)")
+                .HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("role_pkey");
+
+            entity.ToTable("role");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("account_pkey");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("nextval('account_id_seq'::regclass)")
+                .HasColumnName("id");
+            entity.Property(e => e.DepartmentId).HasColumnName("department_id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .HasColumnName("email");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(255)
+                .HasColumnName("full_name");
+            entity.Property(e => e.Login)
+                .HasMaxLength(100)
+                .HasColumnName("login");
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .HasColumnName("password");
+            entity.Property(e => e.Photo).HasColumnName("photo");
+            entity.Property(e => e.RoleId).HasColumnName("role_id");
+            entity.Property(e => e.Telephone)
+                .HasMaxLength(50)
+                .HasColumnName("telephone");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Users)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("fk_unit_id");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("fk_role_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
